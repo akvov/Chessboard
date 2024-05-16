a7604777ed8141cd3830034d87d5c2027fd34f5d
Владимир <kozello98@rambler.ru>
2024-05-16 19:03:54 +0300

    update

--- Chessboard/main.js
+++ Chessboard/main.js
@@ -3,8 +3,7 @@ function colorCells() //а есть ли смысл? можно изначаль
 {
     let evenCol = ["B","D","F","H"]
     let elements = document.querySelectorAll(".cell")
-    console.log("hewwo");
-    console.log(document.querySelector("cell") );
+
     for (let elem of elements)
     {
         let place = elem.id.split("");
@@ -24,7 +23,88 @@ function colorCells() //а есть ли смысл? можно изначаль
 
 function defaultStage()
 {
+    mainBoard = [
+        ['BR','BN','BB','BQ','BK','BB','BN','BR'],
+        ['BP','BP','BP','BP','BP','BP','BP','BP'],
+        ['0','0','0','0','0','0','0','0'],
+        ['0','0','0','0','0','0','0','0'],
+        ['0','0','0','0','0','0','0','0'],
+        ['0','0','0','0','0','0','0','0'],
+        ['WP','WP','WP','WP','WP','WP','WP','WP'],
+        ['WR','WN','WB','WQ','WK','WB','WN','WR']
+        ];
 
+        for (let i = 0; i < 8; i++ )
+            for (let j = 0; j < 8; j++ )
+            {
+                setFigure( [j,i], mainBoard[i][j] )
+            }
 }
 
+
+
+//рисуем/убираем фигуры на доске (img.content)
+function setFigure(coordinates, figure) 
+{
+    let cell_id = coordToDesk(coordinates);
+    let cell = document.getElementById(cell_id);
+    cell.figure = figure
+    cell.firstElementChild.src = "images/" +  (figure != "0" ? figure : "void" ) + ".png";
+ 
+}
+
+//рисуем/убираем точки на доске (img.over)
+function setPoint(coordinates, point) //point: true => point
+{
+    let cell_id = coordToDesk(coordinates);
+    let cell = document.getElementById(cell_id);
+    cell.lastElementChild.src = "images/" +  (point ? "point" : "void" ) + ".png";
+    point ? pointedCells.push(coordinates) : pointedCells.shift();
+}
+function clearPoints()
+{
+    let cellCoord 
+    while (pointedCells.length > 0) 
+        {
+            cellCoord = pointedCells[0];
+            setPoint(cellCoord, false);
+        }
+}
+
+function coordToDesk(coordinates)// 00,77 => a8,h1
+{
+    let numToSymb = ['A','B','C','D','E','F','G','H'];
+    return numToSymb[coordinates[0] ] +  (8-coordinates[1]);
+}
+function deskToCoord(desk) //a8,h1 => 00,77
+{
+    let numToSymb = ['A','B','C','D','E','F','G','H'];
+    return [numToSymb.findIndex(elem => elem == desk[0]) , (8 - Number(desk[1]) ) ]
+}
+
+
+
+function clickOver(elem)
+{
+    console.log("over " + elem.parentNode.id + ' ' + elem.parentNode.figure);
+    setPoint(deskToCoord(elem.parentNode.id), true);
+}
+
+let mainBoard = [
+['0','0','0','0','0','0','0','0'],
+['0','0','0','0','0','0','0','0'],
+['0','0','0','0','0','0','0','0'],
+['0','0','0','0','0','0','0','0'],
+['0','0','0','0','0','0','0','0'],
+['0','0','0','0','0','0','0','0'],
+['0','0','0','0','0','0','0','0'],
+['0','0','0','0','0','0','0','0']
+];
+
+let pointedCells = []; //клетки отмеченные точками; координаты [0-7,0-7]
+
 colorCells();
+
+//setFigure([4,7], "WK" )
+//setFigure([4,7], "0")
+//defaultStage();
